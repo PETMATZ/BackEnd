706f3e4bae203d12f01c4235f6ebc1445e59f523
류희수 <hs201016@gmail.com>
2025-01-10 01:11:18 +0900

    Refactor : 1차적인 컴포넌트 | 서비스 분리 작업 완료!

--- src/main/java/com/petmatz/domain/user/component/PasswordComponent.java
+++ src/main/java/com/petmatz/domain/user/component/PasswordComponent.java
@@ -1,82 +1,27 @@
 package com.petmatz.domain.user.component;
 
-import com.petmatz.api.user.request.SendRepasswordRequestDto;
-import com.petmatz.common.security.utils.JwtExtractProvider;
-import com.petmatz.domain.user.entity.User;
-import com.petmatz.domain.user.info.RepasswordInfo;
-import com.petmatz.domain.user.provider.RePasswordProvider;
-import com.petmatz.domain.user.repository.UserRepository;
-import com.petmatz.domain.user.response.GetMyProfileResponseDto;
-import com.petmatz.domain.user.response.RepasswordResponseDto;
-import com.petmatz.domain.user.response.SendRepasswordResponseDto;
-import com.petmatz.domain.user.service.RePasswordEmailProvider;
+import com.petmatz.domain.user.exception.UserException;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
+
+import static com.petmatz.domain.user.exception.MatchErrorCode.PASSWORD_MISMATCH;
 
 @Component
 @RequiredArgsConstructor
-@Slf4j
 public class PasswordComponent {
 
-    private final UserRepository userRepository;
-    private final RePasswordEmailProvider rePasswordEmailProvider;
-    private final JwtExtractProvider jwtExtractProvider;
-    private PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+    private final PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
-    @Transactional
-    public ResponseEntity<? super SendRepasswordResponseDto> sendRepassword(SendRepasswordRequestDto dto) {
-        try {
-            String accountId = dto.getAccountId();
-            if (!userRepository.existsByAccountId(accountId)) {
-                return GetMyProfileResponseDto.idNotFound();
-            }
-            User user = userRepository.findByAccountId(accountId);
-
-            String rePasswordNum = RePasswordProvider.generatePassword();
-            log.info("Generated Repassword: {}", rePasswordNum);
-
-            boolean isSendSuccess = rePasswordEmailProvider.sendVerificationEmail(accountId, rePasswordNum);
-            if (!isSendSuccess) return SendRepasswordResponseDto.mailSendFail();
-
-            String encodedRePasswordNum = passwordEncoder.encode(rePasswordNum);
-
-            user.updatePassword(encodedRePasswordNum);
-
-        } catch (Exception e) {
-            log.info("임시비밀번호 재설정 실패: {}", e);
-            return SendRepasswordResponseDto.databaseError();
-        }
-        return SendRepasswordResponseDto.success();
+    public String encodePassword(String repasswordNum) {
+        passwordEncoder.encode(repasswordNum);
+        return repasswordNum;
     }
 
-    @Transactional
-    public ResponseEntity<? super RepasswordResponseDto> repassword(RepasswordInfo info) {
-        try {
-            Long userId = jwtExtractProvider.findIdFromJwt();
-            User user = userRepository.findById(userId)
-                    .orElseThrow(() -> new IllegalArgumentException("User not found for ID: " + userId));
-
-            String currentPassword = info.getCurrentPassword();
-
-            boolean isPasswordValid = passwordEncoder.matches(currentPassword, user.getPassword());
-            if (!isPasswordValid) {
-                return RepasswordResponseDto.wrongPassword();
-            }
-            String newPassword = info.getNewPassword();
-
-            String encodedNewPassword = passwordEncoder.encode(newPassword);
-
-            user.updatePassword(encodedNewPassword);
-
-        } catch (Exception e) {
-            log.info("비밀번호 재설정 실패: {}", e);
-            return RepasswordResponseDto.databaseError();
+    public void validatePassword(String currentPassword, String userPassword) {
+        if (!passwordEncoder.matches(currentPassword, userPassword)) {
+            throw new UserException(PASSWORD_MISMATCH);
         }
-        return RepasswordResponseDto.success();
     }
 }
